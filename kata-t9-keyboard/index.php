e732c8545b393dbd48f0dbe768cbd0ac0106f06a
CarlesDeveloperPHP <crmfeina@gmail.com>
2023-09-21 11:25:51 +0200

    @media ok

---
+++ kata-t9-keyboard/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+$keyPushed = "5-3-0-111-1-77-1"; //aqui recoger la entrada
+
+function translate($keyPushed){
+    $msg = "";
+    $countPusheds = 0;
+    $letter ="";
+
+    //crear un arraylist asociativo con los nums a que perrtenece ej 666 m 
+
+    for ($i=0; $i < strlen($keyPushed); $i++) { // esto es mejor un while
+       //aqui busca hasta que encuentre un - y va poniendo en cada paso una lettter y al final al msg
+    }
+}
+
+translate(($keyPushed));
+
+
+
+?>
